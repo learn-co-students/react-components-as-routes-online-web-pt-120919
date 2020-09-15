b02eedaca1b8e475c74995e794633b0397d4cad2
MilanP5G <milan_parmar@hotmail.co.uk>
2020-09-15 16:38:42 +0100

    Done.

---
+++ src/Login.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default class Login extends React.Component {
+  render () {
+    return (
+      <form>
+        <h1>Login</h1>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    )
+  }
+}
